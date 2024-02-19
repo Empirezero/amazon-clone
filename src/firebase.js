58075fc944187cab6653dfd5e731115fca9ca510
dancanngugi936@gmal.com <dancanngugi936@gmail.com>
2024-02-19 18:45:07 +0300

    Amazon home page

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDRRtfF5YpPin3JoJWlz4oijSTSsEuhX1k",
+  authDomain: "clone-5274a.firebaseapp.com",
+  projectId: "clone-5274a",
+  storageBucket: "clone-5274a.appspot.com",
+  messagingSenderId: "969988948864",
+  appId: "1:969988948864:web:82fe4be11731b2c5887765",
+  measurementId: "G-MZENKN8ZG5"
+};
