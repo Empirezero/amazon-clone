b8888b7be82b7bf14f3d333aad073b2061905d6c
dancanngugi936@gmal.com <dancanngugi936@gmail.com>
2024-03-09 18:10:03 +0300

    Amazon clone

--- src/App.js
+++ src/App.js
@@ -2,19 +2,44 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Login from './Login';
 import { BrowserRouter as Router, Routes,Route,Navigate,
 } from "react-router-dom";
 import Checkout from "./Checkout";
+import { useEffect, useState } from "react";
+import { auth } from "./firebase";
+import { useStateValue } from './StateProvider';
 function App() {
+  const [authUser, dispatch] = useStateValue();
+  useEffect(() => {
+    //Will only run once te app componets loads
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The user is >>>", authUser);
+
+      if (authUser) {
+        //The user ust logged in/the user was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //the uset is loged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, [dispatch]);
+
+
   return (
     //BEM
     <Router>
       <div className="app">
         <Routes>
-          <Route
-            path="/Checkout"
-            element={[<Header />,<Checkout/>]}
-          />
+          <Route path="/Login" element={[<Login />, <h1>login page</h1>]} />
+          <Route path="/Checkout" element={[<Header />, <Checkout />]} />
           <Route path="/" element={[<Header />, <Home />]} />
         </Routes>
       </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDRRtfF5YpPin3JoJWlz4oijSTSsEuhX1k",
   authDomain: "clone-5274a.firebaseapp.com",
@@ -6,5 +9,10 @@ const firebaseConfig = {
   storageBucket: "clone-5274a.appspot.com",
   messagingSenderId: "969988948864",
   appId: "1:969988948864:web:82fe4be11731b2c5887765",
-  measurementId: "G-MZENKN8ZG5"
+  measurementId: "G-MZENKN8ZG5",
 };
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+export { db, auth };
